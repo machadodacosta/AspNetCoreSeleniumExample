46cbfd05c5b6ef141f6320b3f55b32c33e9bacd2
machadodacosta <47395294+machadodacosta@users.noreply.github.com>
2019-03-19 16:38:18 -0300

    Update GooglePageExampleTest.cs

--- Exemplo.AspNetCoreSelenium/GooglePageExampleTest.cs
+++ Exemplo.AspNetCoreSelenium/GooglePageExampleTest.cs
@@ -1,7 +1,8 @@
-﻿using Exemplo.AspNetCoreSelenium.Utils;
+using Exemplo.AspNetCoreSelenium.Utils;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 using Xunit;
 
 namespace Exemplo.AspNetCoreSelenium
@@ -12,12 +13,12 @@ namespace Exemplo.AspNetCoreSelenium
         /// Access the site https://www.seleniumhq.org/download/ to get the other drivers
         /// </summary>
         private readonly string driverDirectory = "\\Drivers";
-        private readonly bool openBrowser = true;
+        private readonly bool isHeadless = false;
         private readonly Browser browser = Browser.Chrome;
 
         private readonly string url = "https://www.google.com";
 
-        [Fact]
+        [Fact(DisplayName = "Search by Selenium on Google web site.")]
         public void SearchForSeleniumInGooglePage()
         {
             IWebDriver driver = GetDriver();
@@ -30,23 +31,34 @@ namespace Exemplo.AspNetCoreSelenium
 
             IWebElement buttonClick = driver.FindElement(By.Name("btnK"));
             buttonClick.Submit();
+
+            IWebElement foundLink = driver.FindElement(By.PartialLinkText("Selenium - Web Browser Automation"));
+
+            Assert.NotNull(foundLink);
+
+            driver.Close();
         }
 
-        protected OpenQA.Selenium.Remote.RemoteWebDriver GetDriver()
+        protected RemoteWebDriver GetDriver()
         {
             if (browser == Browser.Chrome)
             {
-                var driverOptions = new ChromeOptions();
-                if (openBrowser)
-                {
-                    (driverOptions as ChromeOptions).AddArgument("--headless");
-                }
+                return GetChromeDriver();
+            }
 
+            return new InternetExplorerDriver(driverDirectory);
+        }
+
+        private RemoteWebDriver GetChromeDriver()
+        {
+            if (isHeadless)
+            {
+                var driverOptions = new ChromeOptions();
+                driverOptions.AddArgument("--headless");
                 return new ChromeDriver(driverDirectory, driverOptions);
             }
 
-            //headless doesn't work for Internet Explorer
-            return new InternetExplorerDriver(driverDirectory);
+            return new ChromeDriver(driverDirectory);
         }
     }
 }
